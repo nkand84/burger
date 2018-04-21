6897572937e3836da272fa85dc7aaa645f59c832
nkand84 <nityabhagi@gmail.com>
2018-04-21 15:19:26 -0700

    db setup done

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
+SELECT * FROM burgers;
