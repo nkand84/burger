2f44f4e47679676ccce51a1a57dc9f74709b81e1
nkand84 <nityabhagi@gmail.com>
2018-04-21 22:21:42 -0700

    in progress

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,9 +6,27 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function (req, res) {
-    res.send("Hello!");
+    // res.send("Hello!");
+    // res.render("index");
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
 });
 
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+      "burger_name"
+    ], [
+      req.body.name
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
 
 
 
